d3159ed8840a55ab3eaf5f376c7f92cc7be2ada3
binitamuskan996 <binitabini05@gmail.com>
2025-12-03 17:38:36 +0530

    add ecomm code

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
