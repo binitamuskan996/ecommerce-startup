375e0e9aa2163fdb04b4cfc2a1e31870ed11b2cf
binitamuskan996 <binitabini05@gmail.com>
2025-12-04 16:12:50 +0530

    add services

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,33 @@
+const productService = require("../services/productService");
+
 const getAllProducts = (req, res) => {
-    res.send("Fetching all products");
+    const result = productService.getAllProductsService();
+    res.send(result);
 };
 
 const getProductById = (req, res) => {
     const { id } = req.params;
-    res.send(`Fetching product with ID: ${id}`);
+    const result = productService.getProductByIdService(id);
+    res.send(result);
 };
 
 const addProduct = (req, res) => {
-    res.send("Adding a new product");
+    const result = productService.addProductService();
+    res.send(result);
 };
+
+// const getAllProducts = (req, res) => {
+//     res.send("Fetching all products");
+// };
+
+// const getProductById = (req, res) => {
+//     const { id } = req.params;
+//     res.send(`Fetching product with ID: ${id}`);
+// };
+
+// const addProduct = (req, res) => {
+//     res.send("Adding a new product");
+// };
 
 module.exports = {
     getAllProducts,
