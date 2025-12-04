212537d1f781fe10f9cade86f5b479ad1d09eeb1
binitamuskan996 <binitabini05@gmail.com>
2025-12-04 15:51:53 +0530

    add controller

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,31 +4,12 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 
 // Fetch all products
-router.get("/", productController.getProducts);
+router.get("/", productController.getAllProducts);
 
 // Fetch a product by ID
-router.get("/:id", productController.getProductsByID);
+router.get("/:id", productController.getProductById);
 
 // Add a new product
-router.post("/", productController.postProducts);
-
-// Put request
-router.put("/:id", productController.editProducts);
-
-// Delete request
-router.delete("/:id", productController.deleteProducts);
-
-// router.get("/", (req, res) => {
-//   res.send("Fetching all products");
-// });
-
-// router.post("/", (req, res) => {
-//   res.send("Adding a new product");
-// });
-
-// router.get("/:id", (req, res) => {
-//   const id = req.params.id;
-//   res.send(`Fetching product with ID: ${id}`);
-// });
+router.post("/", productController.addProduct);
 
 module.exports = router;
